7ce9dff11bd514e23b5960a322b6e0526cfa5f7a
John Sok <98001557+jsok59@users.noreply.github.com>
2025-04-01 00:22:21 -0400

    Add change size button and functionality

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
 
-const mainContainer = document.querySelector('.main-container');
-
 function toggleColor(e) {
-    e.target.style.backgroundColor = "black";
+    e.target.style.backgroundColor = getRandomColor();
 
 }
 
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+  
 
 function createBoard(size = 16){
-    for (let i = 1; i<=16; i++) {
+    for (let i = 1; i<=size; i++) {
         const divRow = document.createElement('div');
         divRow.classList.add('row-container');
 
-        for (let j = 1; j <= 16; j++) {
+        for (let j = 1; j <= size; j++) {
             const divCell = document.createElement('div');
             divCell.classList.add('cell');
             divCell.addEventListener("mouseover", toggleColor);
@@ -28,5 +35,31 @@ function createBoard(size = 16){
 
 }
 
+function createSizeButton() {
+    const button = document.createElement('button');
+    button.classList.add('size-button');
+    button.textContent = "Change Size";
+    button.addEventListener("click", changeBoard)
+    const body = document.querySelector('body');
+    body.insertBefore(button, mainContainer);
 
+}
+
+function changeBoard() {
+    let size;
+    do {
+    size = prompt("Pick a size");
+    } while (size > 100 && size)
+    clearBoard();
+    createBoard(size);
+}
+
+
+function clearBoard() {
+    const rowList = document.querySelectorAll('.row-container');
+    rowList.forEach((row)=>row.remove());
+}
+
+const mainContainer = document.querySelector('.main-container');
+createSizeButton();
 createBoard();
